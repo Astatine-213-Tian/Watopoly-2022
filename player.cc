b05cec2167866a470c317b1420f2d22b2ccf9bb6
xd-judy04 <wangxds002@outlook.com>
2022-07-18 17:19:03 -0400

    add player.cc

---
+++ player.cc
@@ -0,0 +1,12 @@
+#include <vector>
+#include "player.h"
+
+using namespace std;
+
+Player::Player(string name): name{name}, cash{0} {}
+
+string Player::getName() { return name; }
+
+vector<Property *> Player::getProperties() { return properties; }
+
+float Player::getCash() { return cash; }
