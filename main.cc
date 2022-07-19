f75d71792b4ce00c2d46926675476b0fb2e50aec
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-19 19:24:22 -0400

    Remove unused code

--- main.cc
+++ main.cc
@@ -9,21 +9,14 @@ using namespace std;
 int main(int argc, char *argv[]) {
     Controller c;
     if (argc == 1) {
-        c.init();
         c.play();
     } else if (argc == 3) {
         c.load();
         c.play();
     } else if (argc == 2) {
-        istringstream die1{argv[2]};
-        istringstream die2{argv[3]};
-        int roll1;
-        int roll2;
-        die1 >> roll1;
-        die2 >> roll2;
-        // c.g->roll();
+        // TODO testing mode
     } else {
-        cerr << "Incorrect number of command line argumnents." << endl;
+        cerr << "Incorrect number of command line arguments." << endl;
         return 1;
     }
     return 0;
