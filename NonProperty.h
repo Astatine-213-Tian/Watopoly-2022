35baec1cedef07b4397f016c2f898e5ce6f04f53
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-18 23:17:58 -0400

    Remove unnecessary virtual

--- NonProperty.h
+++ NonProperty.h
@@ -2,12 +2,8 @@
 #define _NONPROPERTY_H_
 
 #include "cell.h"
-class Player;
 
-class NonProperty: public Cell {
-public:
-    virtual void passBy(Player &p) = 0;
-    virtual void landOn(Player &p) = 0;
-};
+class Player;
+class NonProperty: public Cell {};
 
 #endif
