d6c35c19d923c0119ba824a314ce84b6333a60dd
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-19 02:14:19 -0400

    Change buy/sell improve param from Property to AcademicBuilding Change string param to const &string

--- gameboard.cc
+++ gameboard.cc
@@ -3,76 +3,114 @@
 #include <memory>
 #include "gameboard.h"
 #include "player.h"
+#include "cell.h"
+#include "property.h"
+#include "error.h"
+#include "academicBuilding.h"
 
 using namespace std;
 
 Player *GameBoard::getCurPlayer() { return curPlayer; }
 
-Player *GameBoard::getPlayer(string name) {
+Player *GameBoard::getPlayer(const string& name) {
 	for (auto i : players) {
 		if (i->getName() == name) return i;
 	}
-	return nullptr;
+    throw NotPlayer{name};
 }
 
-Property *GameBoard::getProperty(string name) {
+Property *GameBoard::getProperty(const string& name) {
 	for (auto i : cells) {
-		if (i->getName() == name) return i;
+		if (i->getName() == name) {
+            return dynamic_cast<Property*>(i);
+        };
 	}
-	return nullptr;
+    throw NotProperty{name};
+}
+
+AcademicBuilding *GameBoard::getAcademicBuilding(const std::string &name) {
+    for (auto i : cells) {
+        if (i->getName() == name) {
+            return dynamic_cast<AcademicBuilding*>(i);
+        };
+    }
+    throw NotAcademicBuilding{name};
 }
 
 void GameBoard::trade(Player &player, double value, Property &property) {
-    if (curPlayer->payMoney(value) && property.getOwner() == &player) {
-        player.receiveMoney(value);
-        curPlayer->addProperty(property);
-        player.removeProperty(property);
-    } else if (!(player.hasProperty(property))) {
-        cout << "Invalid trade." << endl;
-    } else {
-        // bankrupt
+    // TODO encapsulate in player?
+    if (property.getOwner() != &player) {
+        throw NotOwner{player.getName(), property.getName()};
     }
+    curPlayer->payMoney(value);
+    curPlayer->addProperty(property);
+    property.setOwner(curPlayer);
+    player.removeProperty(property);
+    player.receiveMoney(value);
+
+//    if (curPlayer->payMoney(value) && property.getOwner() == &player) {
+//        player.receiveMoney(value);
+//        curPlayer->addProperty(property);
+//        player.removeProperty(property);
+//    } else if (!(player.hasProperty(property))) {
+//        cout << "Invalid trade." << endl;
+//    } else {
+//        // bankrupt
+//    }
 }
 
 void GameBoard::trade(Player &player, Property &p1, Property &p2) {
-    if (p1.getOwner() == curPlayer && p2.getOwner() == &player) {
-        curPlayer->removeProperty(p1);
-	    curPlayer->addProperty(p2);
-	    player.addProperty(p1);
-	    player.removeProperty(p2);
-    } else {
-        cout << "Invalid trade." << endl;
+    // TODO encapsulate in player?
+    if (p1.getOwner() != curPlayer) {
+        throw NotOwner{curPlayer->getName(), p1.getName()};
+    } else if (p2.getOwner() != &player) {
+        throw NotOwner{player.getName(), p2.getName()};
     }
+    curPlayer->removeProperty(p1);
+    curPlayer->addProperty(p2);
+    player.addProperty(p1);
+    player.removeProperty(p2);
+    p1.setOwner(&player);
+    p2.setOwner(curPlayer);
+
+//    if (p1.getOwner() == curPlayer && p2.getOwner() == &player) {
+//        curPlayer->removeProperty(p1);
+//	    curPlayer->addProperty(p2);
+//	    player.addProperty(p1);
+//	    player.removeProperty(p2);
+//    } else {
+//        cout << "Invalid trade." << endl;
+//    }
 }
 
 void GameBoard::trade(Player &player, Property &property, double value) {
-    if (property.getOwner() == curPlayer && player.payMoney(value)){
-        curPlayer->receiveMoney(value);
-        curPlayer->removeProperty(property);
-        player.addProperty(property);
-    } else if (!(curPlayer->hasProperty(property))) {
-        cout << "Invalid trade." << endl;
-    } else {
-        // bankrupt
+    // TODO encapsulate in player?
+    if (property.getOwner() != curPlayer) {
+        throw NotOwner{curPlayer->getName(), property.getName()};
     }
+    player.payMoney(value);
+    player.addProperty(property);
+    curPlayer->receiveMoney(value);
+    curPlayer->removeProperty(property);
+    property.setOwner(&player);
+
+//    if (property.getOwner() == curPlayer && player.payMoney(value)){
+//        curPlayer->receiveMoney(value);
+//        curPlayer->removeProperty(property);
+//        player.addProperty(property);
+//    } else if (!(curPlayer->hasProperty(property))) {
+//        cout << "Invalid trade." << endl;
+//    } else {
+//        // bankrupt
+//    }
 }
 
-void GameBoard::buyImprove(Property &p) {
-	try {
-        Academic ab = p;
-        ab.addImprove();
-    } catch (bad_cast &bc) {
-        cout << "This is not an academic building." << endl;
-    }
+void GameBoard::buyImprove(AcademicBuilding &ab) {
+    // TODO curPlayer->buyImprove(ab) - checkIsMonopolized
 }
 
-void GameBoard::sellImprove(Property &p) {
-    try {
-        Academic ab = p;
-        ab.sellImprove();
-    } catch (bad_cast &bc) {
-        cout << "This is not an academic building." << endl;
-    }
+void GameBoard::sellImprove(AcademicBuilding &ab) {
+    // TODO curPlayer->sellImprove(ab)
 }
 
 void GameBoard::mortgage(Property &p) {
--- gameboard.h
+++ gameboard.h
@@ -6,27 +6,33 @@
 
 class Cell;
 class Property;
+class AcademicBuilding;
 class Player;
+class Dice;
 
 class GameBoard {
     std::vector<Cell *> cells;
     std::vector<Player *> players;
     Player *curPlayer;
+    Dice *dice1;
+    Dice *dice2;
 	bool checkInTimsLine();
 	// map, roll
 
 public:
     void init();
 	void roll(); // save dice val, check dice, check num double
+    void roll(int d1, int d2);
 	void next(); // check can next, 
 	Player *getCurPlayer();
-	Player *getPlayer(std::string name);
-	Property *getProperty(std::string name);
+	Player *getPlayer(const std::string& name);
+	Property *getProperty(const std::string& name);
+    AcademicBuilding *getAcademicBuilding(const std::string& name);
 	void trade(Player &player, double value, Property &property);
 	void trade(Player &player, Property &p1, Property &p2);
 	void trade(Player &player, Property &property, double value);
-	void buyImprove(Property &p);
-	void sellImprove(Property &p);
+	void buyImprove(AcademicBuilding &ab);
+	void sellImprove(AcademicBuilding &ab);
 	void mortgage(Property &p);
 	void unmortgage(Property &p);
 	void assets(Player &p);
