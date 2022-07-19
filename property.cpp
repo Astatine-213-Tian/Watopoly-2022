97f1d5198af6201a08717e0f868c7653f0d278af
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-18 20:59:55 -0400

    Complete Property

--- property.cpp
+++ property.cpp
@@ -1,5 +1,6 @@
-#include "property.h"
 #include <utility>
+#include <iostream>
+#include "property.h"
 #include "player.h"
 
 Property::Property(std::string name, float cost): Cell{std::move(name)}, cost{cost}, owner{nullptr} {}
@@ -9,9 +10,32 @@ void Property::setOwner(Player *p) { owner = p; }
 
 float Property::getCost() const { return cost; }
 
-void Property::passBy(Player &p) { return; }
+void Property::passBy(Player &p) { }
 
 void Property::landOn(Player &p) {
-    float rent = calculateRent(p);
     if (&p == owner) return;
+
+    if (!owner) {
+        bool buy = false;
+        std::cout << "Do you want to buy the property? (y/n)" << std::endl;
+        std::string ans;
+        while (std::cin >> ans) {
+            if (ans == "Y" || ans == "y" || ans == "Yes" || ans == "yes") {
+                buy = true;
+                break;
+            } else if (ans == "N" || ans == "n" || ans == "No" || ans == "no") {
+                buy = false;
+                break;
+            } else {
+                std::cout << "Please answer with y/n" << std::endl;
+            }
+        }
+        if (!buy) return;
+    }
+
+    float rent = calculateRent(p);
+    if (p.payMoney(rent)) {
+        owner = &p;
+        owner->receiveMoney(rent);
+    }
 }
