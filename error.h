c951ded1511f5120c9580c4b39282bb9c92f231a
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-19 00:09:55 -0400

    Add ctor for error message

--- error.h
+++ error.h
@@ -2,11 +2,23 @@
 #define WATOPOLY__ERROR_H_
 
 #include <stdexcept>
+#include <utility>
+
+class Player;
+class Property;
 
 class Error : public std::exception {};
 
-// throw when player is not authorized to do sth
-class NotOwner : public Error {};
+// throw when player is not the owner of a property
+class NotOwner : public Error {
+  std::string playerName;
+  std::string propertyName;
+ public:
+  NotOwner(std::string playerName, std::string propertyName);
+};
+
+NotOwner::NotOwner(std::string playerName, std::string propertyName) : playerName{std::move(playerName)}, propertyName{std::move(propertyName)} {}
+
 // throw when player try to improve an un-monopolized block
 class NotMonopolized: public Error {};
 // throw when a block reaches maximum improvement number
