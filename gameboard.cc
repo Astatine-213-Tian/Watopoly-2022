c74ce5b3a202eddf9bb6c7794830e14b1650baf4
xd-judy04 <wangxds002@outlook.com>
2022-07-20 10:55:04 -0400

    removed list of properties in Player

--- gameboard.cc
+++ gameboard.cc
@@ -15,6 +15,7 @@
 #include "gym.h"
 #include "goToTims.h"
 #include "coop.h"
+#include "action.h"
 
 using namespace std;
 
@@ -94,7 +95,11 @@ void GameBoard::roll() {
                 cells[dest]->passBy(*curPlayer);
                 curPlayer->move(dest);
             }
-            cells[curPlayer->getLocation()]->landOn(*curPlayer);
+            try {
+                cells[curPlayer->getLocation()]->landOn(*curPlayer);
+            } catch (sendToTims &) {
+                // send to tims
+            }
 
             if (roll1 == roll2) {
                 curPlayer->setRollState(true);
@@ -162,12 +167,15 @@ void GameBoard::trade(Player &player, double value, Property &property) {
     if (property.getOwner() != &player) {
         throw NotOwner{player.getName(), property.getName()};
     }
-    curPlayer->payMoney(value);
-    curPlayer->addProperty(property);
     property.setOwner(curPlayer);
-    player.removeProperty(property);
-    player.receiveMoney(value);
+    
+    //check bankrupt
+    curPlayer->payMoney(value);
 
+    // curPlayer->addProperty(property);
+    // player.removeProperty(property);
+    // player.receiveMoney(value);
+    
 //    if (curPlayer->payMoney(value) && property.getOwner() == &player) {
 //        player.receiveMoney(value);
 //        curPlayer->addProperty(property);
@@ -186,13 +194,14 @@ void GameBoard::trade(Player &player, Property &p1, Property &p2) {
     } else if (p2.getOwner() != &player) {
         throw NotOwner{player.getName(), p2.getName()};
     }
-    curPlayer->removeProperty(p1);
-    curPlayer->addProperty(p2);
-    player.addProperty(p1);
-    player.removeProperty(p2);
     p1.setOwner(&player);
     p2.setOwner(curPlayer);
 
+    // curPlayer->removeProperty(p1);
+    // curPlayer->addProperty(p2);
+    // player.addProperty(p1);
+    // player.removeProperty(p2);
+    
 //    if (p1.getOwner() == curPlayer && p2.getOwner() == &player) {
 //        curPlayer->removeProperty(p1);
 //	    curPlayer->addProperty(p2);
@@ -208,11 +217,13 @@ void GameBoard::trade(Player &player, Property &property, double value) {
     if (property.getOwner() != curPlayer) {
         throw NotOwner{curPlayer->getName(), property.getName()};
     }
-    player.payMoney(value);
-    player.addProperty(property);
-    curPlayer->receiveMoney(value);
-    curPlayer->removeProperty(property);
+    // player.addProperty(property);
+    // curPlayer->receiveMoney(value);
+    // curPlayer->removeProperty(property);
     property.setOwner(&player);
+
+    //check bankrupt
+    player.payMoney(value);
 
 //    if (property.getOwner() == curPlayer && player.payMoney(value)){
 //        curPlayer->receiveMoney(value);
@@ -258,8 +269,14 @@ void GameBoard::assets(Player &p) {
     cout << "Player " << p.getName() << endl;
     cout << "Current Saving: $" << p.getCash() << endl;
     cout << "Properties: " << endl;
-    for (auto i : p.getProperties()) {
-        cout << i->getName() << endl;
+    for (auto &i : cells) {
+        try {
+            if (dynamic_cast<Property &> (*i).getOwner() == &p) {
+                cout << i->getName() << endl;
+            }
+        } catch (bad_cast &) {
+            continue;
+        }
     }
 }
 
--- player.h
+++ player.h
@@ -8,7 +8,7 @@ class AcademicBuilding;
 
 class Player {
     std::string name;
-    std::vector<Property *> properties;
+    //std::vector<Property *> properties;
     double cash;
     int numCup;
     int curLocation;
@@ -19,16 +19,16 @@ class Player {
 public:
     Player(std::string name);
     std::string getName() const;
-    std::vector<Property *> getProperties() const;
+    //std::vector<Property *> getProperties() const;
     double getCash() const;
     int getNumCup() const;
     void move(int index);
     void useCup();
     bool payMoney(double value);
     void receiveMoney(double value);
-    void addProperty(Property &p);
-    void removeProperty(Property &p);
-    bool hasProperty(Property &p) const;
+    // void addProperty(Property &p);
+    // void removeProperty(Property &p);
+    // bool hasProperty(Property &p) const;
     bool inTims() const;
     void setToTimsLine();
     void removeFromTimsLine();
