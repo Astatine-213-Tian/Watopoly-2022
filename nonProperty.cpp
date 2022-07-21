ff09fa556c96e13b678820b7eb3e87df5a4bdefb
Astatine-213-Tian <astatine213.tian@gmail.com>
2022-07-21 18:29:55 -0400

    delete unneccesary function

--- nonProperty.cpp
+++ nonProperty.cpp
@@ -1,5 +1,3 @@
 #include "nonProperty.h"
 
 NonProperty::NonProperty(const std::string&name): Cell{name} {}
-
-double NonProperty::getValue() const { return 0; }
--- nonProperty.h
+++ nonProperty.h
@@ -6,7 +6,6 @@
 class NonProperty: public Cell {
  public:
   explicit NonProperty(const std::string&);
-  virtual double getValue() const override;
 };
 
 #endif
