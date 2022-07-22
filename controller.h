477a208e6a4ae059fe7d24026e723c6f14f4a0d1
xd-judy04 <wangxds002@outlook.com>
2022-07-22 01:16:53 -0400

    add setGameBoard

--- controller.h
+++ controller.h
@@ -22,6 +22,7 @@ class Controller {
 
  public:
   Controller();
+  void setGameBoard(GameBoard &gb);
 //  bool askPlayerTradeResponse(Player *p);
   void play();
   void load(const std::string& filename);
